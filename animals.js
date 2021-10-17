91f81da7637a8f2c770f370f36cc474d5c786eb2
VVestin <weston.metzler@gmail.com>
2021-10-17 11:59:27 -0600

    Create the general battle system and add Ant, Beaver, Cricket

---
+++ animals.js
@@ -0,0 +1,100 @@
+class Animal {
+   constructor(attack, health, level = 1, food = null) {
+      this.attack = attack
+      this.health = health
+      this.level = level
+      this.food = food
+   }
+
+   attackFront(actionQueue, mySquad, enemySquad) {
+      actionQueue.push(
+         enemySquad.roster[0].hurt.bind(
+            enemySquad.roster[0],
+            actionQueue,
+            enemySquad,
+            mySquad,
+            this.attack
+         )
+      )
+   }
+
+   hurt(actionQueue, mySquad, enemySquad, damage) {
+      console.log(this, 'takes', damage, 'damage')
+      this.health -= damage
+      if (this.health <= 0) this.faint(actionQueue, mySquad, enemySquad)
+   }
+
+   faint(actionQueue, mySquad, enemySquad) {
+      mySquad.roster[mySquad.roster.indexOf(this)] = null
+   }
+
+   startBattle() {}
+
+   toString() {
+      return `[${this.constructor.name} ${this.attack}/${this.health}]`
+   }
+}
+
+class Ant extends Animal {
+   attack = 2
+   health = 1
+
+   faint(actionQueue, mySquad, enemySquad) {
+      super.faint(actionQueue, mySquad, enemySquad)
+      // someday buffFriend should be available elsewhere?
+      const buffFriend = () => {
+         const target = mySquad.getRandomTarget()
+         if (target != null) {
+            target.attack += 2 * this.level
+            target.health += this.level
+         }
+      }
+      actionQueue.push(buffFriend)
+   }
+}
+
+class Beaver extends Animal {
+   attack = 2
+   health = 2
+}
+
+class Cricket {
+   attack = 1
+   health = 2
+
+   faint(actionQueue, mySquad, enemySquad) {
+      const myIndex = mySquad.roster.indexOf(this)
+      super.faint(actionQueue, mySquad, enemySquad)
+      actionQueue.push(
+         mySquad.spawnUnit.bind(
+            mySquad,
+            myIndex,
+            new DeadCricket(this.level, this.level)
+         )
+      )
+   }
+}
+
+class DeadCricket {}
+
+class Mosquito {
+   attack = 2
+   health = 2
+
+   startBattle(actionQueue, mySquad, enemySquad) {
+      const target = enemySquad.getRandomTarget()
+      actionQueue.push(
+         target.hurt.bind(target, actionQueue, mySquad, enemySquad, this.level)
+      )
+   }
+}
+
+module.exports = { Ant, Beaver, Cricket, DeadCricket }
+
+// const tier1s = [Ant, Beaver]
+//
+// let a = new Beaver()
+// console.log(a instanceof Animal)
+// let b = Object.assign(Object.create(Beaver.prototype), a)
+// console.log(b)
+// console.log(b instanceof Beaver)
---
+++ sim.js
@@ -0,0 +1,84 @@
+const Animals = require('./animals')
+
+goat_squad = {
+   wins: 10,
+   hearts: 1,
+   turn: 16,
+   roster: [
+      { animal: 'dodo', attack: 32, health: 34, level: 3 },
+      { animal: 'tiger', attack: 8, health: 7, level: 1 },
+      { animal: 'leopard', attack: 17, health: 15, level: 3 },
+      { animal: 'turtle', attack: 13, health: 15, level: 1.5, food: 'steak' },
+      { animal: 'monkey', attack: 37, health: 37, level: 2, food: 'melon' },
+   ],
+}
+
+class Squad {
+   constructor(roster, wins = 0, hearts = 4, turn = 1) {
+      this.roster = roster
+      this.wins = wins
+      this.hearts = hearts
+      this.turn = turn
+   }
+
+   getRandomTarget() {
+      const eligible = this.roster.filter(a => a)
+      if (eligible.length == 0) return null
+      return eligible[Math.floor(Math.random() * eligible.length)]
+   }
+
+   spawnUnit(index, spawn) {
+      if (this.roster.length < 5) this.roster.splice(index, 0, spawn)
+   }
+}
+
+const printSquads = (lSquad, rSquad) => {
+   console.log(
+      '{',
+      lSquad.roster.map(String).reverse().join(' '),
+      '} vs {',
+      lSquad.roster.map(String).join(' '),
+      '}'
+   )
+}
+
+const clone = o => Object.assign(Object.create(Object.getPrototypeOf(o)), o)
+
+const simulateBattle = (lSquad, rSquad) => {
+   printSquads(lSquad, rSquad)
+   const actionQueue = []
+   // execute start of battle powers
+   lSquad.roster.forEach(a => a.startBattle(actionQueue, lSquad, rSquad))
+   rSquad.roster.forEach(a => a.startBattle(actionQueue, rSquad, lSquad))
+
+   let turnCount = 1
+   do {
+      let a
+      while ((a = actionQueue.shift())) {
+         console.log('executing', a)
+         a()
+      }
+      lSquad.roster = lSquad.roster.filter(x => x)
+      rSquad.roster = rSquad.roster.filter(x => x)
+      if (lSquad.roster.length == 0 || rSquad.roster.length == 0) continue
+
+      console.log(`\nturn ${turnCount++}`)
+      printSquads(lSquad, rSquad)
+      lSquad.roster[0].attackFront(actionQueue, lSquad, rSquad)
+      rSquad.roster[0].attackFront(actionQueue, rSquad, lSquad)
+   } while (actionQueue.length > 0)
+
+   console.log()
+   console.log('The Final Squads are:')
+   printSquads(lSquad, rSquad)
+   console.log('The Outcome is:')
+   if (lSquad.roster.length && !rSquad.roster.length)
+      console.log('Left Wins :)')
+   else if (!lSquad.roster.length && rSquad.roster.length)
+      console.log('Right Wins :(')
+   else console.log('Tie :|')
+}
+
+const exSquad1 = new Squad([new Animals.Beaver(), new Animals.Ant()])
+const exSquad2 = new Squad([new Animals.Ant(), new Animals.Beaver()])
+simulateBattle(exSquad1, exSquad2)
